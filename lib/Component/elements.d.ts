d4984f8314fd60c251572f6df916b165ae5afcc6
Karl MARQUES <kmarques@vetixy.com>
2022-12-06 05:46:09 +0100

    add lib folder for github package

---
+++ lib/Component/elements.d.ts
@@ -0,0 +1,100 @@
+/**
+ * console-feed
+ */
+export declare const Root: import('@emotion/styled-base').StyledComponent<
+  import('react').DetailedHTMLProps<
+    import('react').HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >,
+  Pick<
+    import('react').ClassAttributes<HTMLDivElement> &
+      import('react').HTMLAttributes<HTMLDivElement>,
+    keyof import('react').HTMLAttributes<HTMLDivElement>
+  >,
+  object
+>
+/**
+ * console-message
+ */
+export declare const Message: import('@emotion/styled-base').StyledComponent<
+  import('react').DetailedHTMLProps<
+    import('react').HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >,
+  Pick<
+    import('react').ClassAttributes<HTMLDivElement> &
+      import('react').HTMLAttributes<HTMLDivElement>,
+    keyof import('react').HTMLAttributes<HTMLDivElement>
+  > & {
+    theme: object
+  },
+  object
+>
+/**
+ * message-icon
+ */
+export declare const Icon: import('@emotion/styled-base').StyledComponent<
+  import('react').DetailedHTMLProps<
+    import('react').HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >,
+  Pick<
+    import('react').ClassAttributes<HTMLDivElement> &
+      import('react').HTMLAttributes<HTMLDivElement>,
+    keyof import('react').HTMLAttributes<HTMLDivElement>
+  > & {
+    theme: object
+  },
+  object
+>
+/**
+ * message-amount
+ */
+export declare const AmountIcon: import('@emotion/styled-base').StyledComponent<
+  import('react').DetailedHTMLProps<
+    import('react').HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >,
+  Pick<
+    import('react').ClassAttributes<HTMLDivElement> &
+      import('react').HTMLAttributes<HTMLDivElement>,
+    keyof import('react').HTMLAttributes<HTMLDivElement>
+  > & {
+    theme: object
+  },
+  object
+>
+/**
+ * timestamp
+ */
+export declare const Timestamp: import('@emotion/styled-base').StyledComponent<
+  import('react').DetailedHTMLProps<
+    import('react').HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >,
+  Pick<
+    import('react').ClassAttributes<HTMLDivElement> &
+      import('react').HTMLAttributes<HTMLDivElement>,
+    keyof import('react').HTMLAttributes<HTMLDivElement>
+  > & {
+    theme: object
+  },
+  object
+>
+/**
+ * console-content
+ */
+export declare const Content: import('@emotion/styled-base').StyledComponent<
+  import('react').DetailedHTMLProps<
+    import('react').HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >,
+  Pick<
+    import('react').ClassAttributes<HTMLDivElement> &
+      import('react').HTMLAttributes<HTMLDivElement>,
+    keyof import('react').HTMLAttributes<HTMLDivElement>
+  > & {
+    theme: object
+  },
+  object
+>
---
+++ lib/Component/react-inspector/elements.d.ts
@@ -0,0 +1,60 @@
+/**
+ * Object root
+ */
+export declare const Root: import('@emotion/styled-base').StyledComponent<
+  import('react').DetailedHTMLProps<
+    import('react').HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >,
+  Pick<
+    import('react').ClassAttributes<HTMLDivElement> &
+      import('react').HTMLAttributes<HTMLDivElement>,
+    keyof import('react').HTMLAttributes<HTMLDivElement>
+  >,
+  object
+>
+/**
+ * Table
+ */
+export declare const Table: import('@emotion/styled-base').StyledComponent<
+  import('react').DetailedHTMLProps<
+    import('react').HTMLAttributes<HTMLSpanElement>,
+    HTMLSpanElement
+  >,
+  Pick<
+    import('react').ClassAttributes<HTMLSpanElement> &
+      import('react').HTMLAttributes<HTMLSpanElement>,
+    keyof import('react').HTMLAttributes<HTMLSpanElement>
+  >,
+  object
+>
+/**
+ * HTML
+ */
+export declare const HTML: import('@emotion/styled-base').StyledComponent<
+  import('react').DetailedHTMLProps<
+    import('react').HTMLAttributes<HTMLSpanElement>,
+    HTMLSpanElement
+  >,
+  Pick<
+    import('react').ClassAttributes<HTMLSpanElement> &
+      import('react').HTMLAttributes<HTMLSpanElement>,
+    keyof import('react').HTMLAttributes<HTMLSpanElement>
+  >,
+  object
+>
+/**
+ * Object constructor
+ */
+export declare const Constructor: import('@emotion/styled-base').StyledComponent<
+  import('react').DetailedHTMLProps<
+    import('react').HTMLAttributes<HTMLSpanElement>,
+    HTMLSpanElement
+  >,
+  Pick<
+    import('react').ClassAttributes<HTMLSpanElement> &
+      import('react').HTMLAttributes<HTMLSpanElement>,
+    keyof import('react').HTMLAttributes<HTMLSpanElement>
+  >,
+  object
+>
